4b03afcf3920b554c1c6f698eff29579c02dec56
victor andre <victor27andre@gmail.com>
2020-03-21 23:37:58 -0300

    implementando array fifo

--- src/app/pages/fifo/fifo.page.ts
+++ src/app/pages/fifo/fifo.page.ts
@@ -19,21 +19,27 @@ export class FifoPage implements OnInit {
 
   ) { }
 
+  // this.kernel.processo = array de processos
+  // this.processador.cores = array de nucleos nucleos
 
   ngOnInit() {
-    console.log(this.menuservice.menu);
+    // console.log(this.menuservice.menu); // mostra as opçoes do menu
     this.kernel.generateProcess(this.menuservice.menu.numeroProcesso); // 10 - Starta os primeiros processos
     this.processador.gerarCores(this.menuservice.menu.core); // 4 - Gera os nucleos
+    console.log(this.processador.cores);
+    console.log(this.kernel.processo);
   }
 
   // Movendo processos
   moveProcess() {
-    this.processador.cores.forEach((element, index) => {
-      if (!element.process_id) { // Separa o vetor que esteja vazio
-        this.processador.cores.splice(index) ; // nesse caso ele esta apagando, nesse caso deve jogar os processos para os cores
+    this.processador.cores.forEach((elementProcessador, index) => {
+      if (!elementProcessador.process_id) { // Separa o vetor que esteja vazio
+        // this.processador.cores.splice(index) ; // nesse caso ele esta apagando, nesse caso deve jogar os processos para os cores
+        this.processador.cores.splice(index, 1, this.kernel.processo[0]); // adiciona o processo no nucleo da array do kernel
+        this.kernel.processo.splice(0, 1); // retira o primeiro
       }
     });
-    console.log(this.kernel.processo);
+    // console.log(this.kernel.processo);
   }
 
   print() {
@@ -43,6 +49,7 @@ export class FifoPage implements OnInit {
     // console.log('//////////////////////');
     this.moveProcess();
     console.log(this.processador.cores);
+    console.log(this.kernel.processo);
 
   }
 
